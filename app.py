18c77fa6b5595b94766765f8b1a8a1b40ab83bd8
phette23 <phette23@gmail.com>
2023-10-30 10:34:02 -0700

    python formatting & fix long form of cli flags

--- app.py
+++ app.py
@@ -6,19 +6,57 @@ import sys
 
 import requests
 
-examples = '''Examples:
+examples = """Examples:
   python app.py -s -f > data/out.csv # create a CSV of all PMs & Chairs
   python app.py --staff --json > data/stf.json # write complete staff search JSON to file
   python app.py --pm # print only PM staff to stdout
-'''
-parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description='Pull program chair, program manager, and studio manager information from the Portal People Directory. Writes CSV text to stdout by default.', epilog=examples)
-parser.add_argument("-s, --staff", dest="staff", action="store_true", help='whether to search staff profiles')
+"""
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description="Pull program chair, program manager, and studio manager information from the Portal People Directory. Writes CSV text to stdout by default.",
+    epilog=examples,
+)
+parser.add_argument(
+    "-s",
+    "--staff",
+    dest="staff",
+    action="store_true",
+    help="whether to search staff profiles",
+)
 group = parser.add_mutually_exclusive_group()
-group.add_argument("--sm", dest="sm", action="store_true", help='search staff but only for Studio Managers')
-group.add_argument("--pm", dest="pm", action="store_true", help='search staff but only for Program Managers')
-parser.add_argument("-f, --faculty", dest="faculty", action="store_true", help='whether to search faculty profiles')
-parser.add_argument("-n, --no-header", dest="no_header", action="store_true", help='omit the CSV header row')
-parser.add_argument("-j, --json", dest="json", action="store_true", help='write full JSON data from Portal to stdout. Only works with one of --staff or --faculty.')
+group.add_argument(
+    "--sm",
+    dest="sm",
+    action="store_true",
+    help="search staff but only for Studio Managers",
+)
+group.add_argument(
+    "--pm",
+    dest="pm",
+    action="store_true",
+    help="search staff but only for Program Managers",
+)
+parser.add_argument(
+    "-f",
+    "--faculty",
+    dest="faculty",
+    action="store_true",
+    help="whether to search faculty profiles",
+)
+parser.add_argument(
+    "-n",
+    "--no-header",
+    dest="no_header",
+    action="store_true",
+    help="omit the CSV header row",
+)
+parser.add_argument(
+    "-j",
+    "--json",
+    dest="json",
+    action="store_true",
+    help="write full JSON data from Portal to stdout. Only works with one of --staff or --faculty.",
+)
 args = parser.parse_args()
 
 if args.staff and args.faculty and args.json:
@@ -29,9 +67,9 @@ headers = {
     "accept": "application/json",
     "content-type": "application/json",
 }
-manager = re.compile('[a-z]* manager', flags=re.I)
-pgram = re.compile(' Program', flags=re.I)
-writer = csv.writer(sys.stdout, delimiter='\t')
+manager = re.compile("[a-z]* manager", flags=re.I)
+pgram = re.compile(" Program", flags=re.I)
+writer = csv.writer(sys.stdout, delimiter="\t")
 
 
 def pm(p):
@@ -45,17 +83,17 @@ def pm(p):
     # parse out the academic program, positions look like this
     # "Program Manager: Humanities & Sciences, Writing & Literature and Graduate Comics, Humanities and Sciences"
     # so we ignore everything _before_ the first comma & _after_ the last comma
-    pos_list = position.split(', ')
+    pos_list = position.split(", ")
     # handle Kris McGhee exception: programs were split with a comma & not an "and"
     # https://portal.cca.edu/people/kris.mcghee/
     if len(pos_list) == 4:
-        program = re.sub('.*:', '', '; '.join(pos_list[1:3]))
+        program = re.sub(".*:", "", "; ".join(pos_list[1:3]))
     else:
-        program = position.split(', ')[1].replace(" and ", "; ")
+        program = position.split(", ")[1].replace(" and ", "; ")
     position = manager.match(position)[0]
 
     # some people don't have emails? but everyone has a username
-    return [ p["full_name"], p["username"] + '@cca.edu', position, program ]
+    return [p["full_name"], p["username"] + "@cca.edu", position, program]
 
 
 def sm(p):
@@ -64,10 +102,18 @@ def sm(p):
     # program is hidden in first position somewhere in a wildly inconsistent manner
     # but if we can strip out "Studio Manager" and "Studio Operations" then program
     # is _usually_ the only thing left
-    program = p["positions"][0].replace(', Studio Operations', '').replace('Studio Manager', '').replace('Studio Operations Manager', '').strip(',').strip(' -').strip()
+    program = (
+        p["positions"][0]
+        .replace(", Studio Operations", "")
+        .replace("Studio Manager", "")
+        .replace("Studio Operations Manager", "")
+        .strip(",")
+        .strip(" -")
+        .strip()
+    )
 
     # some people don't have emails? but everyone has a username
-    return [ p["full_name"], p["username"] + '@cca.edu', 'Studio Manager', program ]
+    return [p["full_name"], p["username"] + "@cca.edu", "Studio Manager", program]
 
 
 def chair(p):
@@ -85,26 +131,32 @@ def chair(p):
             if "." in pos:
                 position = pos.split(". ")[0]
                 program = pos.split(", ")[1]
-                program = pgram.sub('', program)
+                program = pgram.sub("", program)
                 programs.add(program)
             else:
                 position = pos.split(", ")[0]
                 program = pos.split(", ")[1]
                 # trim " Program" off the end of string
-                program = pgram.sub('', program)
+                program = pgram.sub("", program)
                 programs.add(program)
 
-    return [ p["full_name"], p["username"] + '@cca.edu', position, '; '.join(list(programs)) ]
+    return [
+        p["full_name"],
+        p["username"] + "@cca.edu",
+        position,
+        "; ".join(list(programs)),
+    ]
 
 
 def handle_json(d):
     if args.json:
-            print(json.dumps(d, indent=2))
-            exit(0)
+        print(json.dumps(d, indent=2))
+        exit(0)
+
 
 # if we're printing to CSV, add a header row
 if not args.json and not args.no_header:
-    writer.writerow(['Name', 'Email', 'Role', 'Program(s)'])
+    writer.writerow(["Name", "Email", "Role", "Program(s)"])
 
 # staff: get program managers
 if args.staff or args.sm or args.pm:
@@ -135,15 +187,20 @@ if args.staff or args.sm or args.pm:
         handle_json(data)
         for result in data["hits"]["hits"]:
             person = result["_source"]
-            if (args.staff or args.sm) and person['staff_primary_department'].lower() == 'studio operations':
+            if (args.staff or args.sm) and person[
+                "staff_primary_department"
+            ].lower() == "studio operations":
                 writer.writerow(sm(person))
-            elif (args.staff or args.pm):
+            elif args.staff or args.pm:
                 for position in person["positions"]:
-                    if "program manager" in position.lower() or "senior manager" in position.lower():
+                    if (
+                        "program manager" in position.lower()
+                        or "senior manager" in position.lower()
+                    ):
                         writer.writerow(pm(person))
                         break
     else:
-        print('ERROR: HTTP {}'.format(r.status_code))
+        print("ERROR: HTTP {}".format(r.status_code))
         print(r.text)
         r.raise_for_status()
 
@@ -180,6 +237,6 @@ if args.faculty:
                     writer.writerow(chair(person))
                     break
     else:
-        print('ERROR: HTTP {}'.format(r.status_code))
+        print("ERROR: HTTP {}".format(r.status_code))
         print(r.text)
         r.raise_for_status()
